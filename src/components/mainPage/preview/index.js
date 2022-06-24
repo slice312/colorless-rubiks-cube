c59d0ff4b94dcf11d50ee4a0cff041d2bf97e05e
Talgat <tairovtalgat@gmail.com>
2022-06-24 20:43:20 +0600

    added concrete training layout page

--- src/components/mainPage/preview/index.js
+++ src/components/mainPage/preview/index.js
@@ -3,5 +3,6 @@ import {ModalOrder} from "/src/components/modalOrder";
 
 export const renderPreview = () => {
     const btnOpen = document.getElementById("btn-discuss-issue");
-    btnOpen.onclick = ModalOrder.openModal;
+    if (btnOpen)
+        btnOpen.onclick = ModalOrder.openModal;
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import "./styles/index.scss";
 import {renderHeaderNav} from "./components/header-nav";
-import {renderPreview} from "./components/mainPage/preview";
+import {renderPreview} from "/src/components/mainPage/preview";
 import {ModalOrder} from "./components/modalOrder";
 
 
-const render = () => {
+export const render = () => {
     renderHeaderNav();
     renderPreview();
+
+    const btnTrainingItemMore = document.getElementById("btn-training-item-more");
+    if (btnTrainingItemMore) {
+        btnTrainingItemMore.onclick = () => {
+            window.location.href = "./training.html";
+        }
+    }
+
+
     ModalOrder.render();
 };
 
 
 document.addEventListener("DOMContentLoaded", render);
+
