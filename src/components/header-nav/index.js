c3854b0e4a02dfb39f3548e2bb98bad7fe56dae6
Talgat <tairovtalgat@gmail.com>
2022-06-27 19:00:48 +0600

    set highlight active nav-link

--- src/components/header-nav/index.js
+++ src/components/header-nav/index.js
@@ -11,5 +11,22 @@ export const renderHeaderNav = () => {
     const closeBtn = document.getElementById("burger-close-btn");
     burgerBtn.onclick = toggleMenu;
     closeBtn.onclick = toggleMenu;
+    setActiveNavLink();
 };
 
+
+
+const setActiveNavLink = () => {
+    const {pathname} = new URL(window.location.href);
+
+    let activeLink = null;
+    if (pathname === "/")
+        activeLink = document.getElementById("nav-link-consultation");
+    else if (pathname === "/services.html")
+        activeLink = document.getElementById("nav-link-services");
+    else if (pathname === "/training-list.html")
+        activeLink = document.getElementById("nav-link-training-list");
+
+
+    activeLink?.classList.add("menu__link_active");
+};
